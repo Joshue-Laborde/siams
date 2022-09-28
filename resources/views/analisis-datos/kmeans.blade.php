1552118da09a660df86240e83b158a95f0c0fe9d
Joshue-Laborde <joshuelab@gmail.com>
2022-09-27 23:23:18 -0500

    Nuevos valores en kmeans

--- resources/views/analisis-datos/kmeans.blade.php
+++ resources/views/analisis-datos/kmeans.blade.php
@@ -61,8 +61,6 @@
                                             <option value=6>6</option>
                                             <option value=7>7</option>
                                             <option value=8>8</option>
-                                            <option value=9>9</option>
-                                            <option value=10>10</option>
                                         </select><br><br>
                                     </div>
                                 </div>
@@ -281,7 +279,79 @@
                                         document.getElementById("graphic3").src =
                                             "../img/agrupacion/k2Tempfinal.png";
                                     })
-                                    break;
+                                break;
+                                case '3':
+                                    swal.fire({
+                                        title: "Ejecutando algoritmo...",
+                                        html: '',
+                                        timer: 2000,
+                                        timerProgressBar: true,
+                                        showConfirmButton: false,
+                                        animation: true,
+                                        onOpen: () => {
+                                            Swal.showLoading()
+                                        }
+                                    }).then((result) => {
+                                        cent.value =
+                                            ' [[0.63835228 0.6644259 ] [0.49409968 0.32376327] [0.83506358 0.65454649]]'
+                                        sil.value = '0.833'
+                                        //Gráfica 1
+                                        document.getElementById("Title_grafic1")
+                                            .innerHTML =
+                                            "Distribución de los puntos";
+                                        document.getElementById("graphic").src =
+                                            "../img/agrupacion/k2Temppuntos.png";
+                                        //Gráfica 2
+                                        document.getElementById("Title_grafic2")
+                                            .innerHTML =
+                                            "Gráfica Elbow";
+                                        document.getElementById("plot").src =
+                                            "../img/agrupacion/k2Tempcodo.png?";
+
+                                        //Gráfica 3
+                                        document.getElementById("Title_grafic3")
+                                            .innerHTML =
+                                            "Gráfica de las Agrupaciones";
+                                        document.getElementById("graphic3").src =
+                                            "../img/agrupacion/k3Tempfinal.png";
+                                    })
+                                break;
+                                case '8':
+                                    swal.fire({
+                                        title: "Ejecutando algoritmo...",
+                                        html: '',
+                                        timer: 2000,
+                                        timerProgressBar: true,
+                                        showConfirmButton: false,
+                                        animation: true,
+                                        onOpen: () => {
+                                            Swal.showLoading()
+                                        }
+                                    }).then((result) => {
+                                        cent.value =
+                                            '[[0.64818914 0.6640626 ][0.47616086 0.33032554][0.83521609 0.66141524][0. 0.6617893 ][0.52870544 0.65831159][0.68362369 0.32408027][0.48861789 0.97826087][0.48863323 0.2209251 ]] '
+                                        sil.value = '0.826'
+                                        //Gráfica 1
+                                        document.getElementById("Title_grafic1")
+                                            .innerHTML =
+                                            "Distribución de los puntos";
+                                        document.getElementById("graphic").src =
+                                            "../img/agrupacion/k2Temppuntos.png";
+                                        //Gráfica 2
+                                        document.getElementById("Title_grafic2")
+                                            .innerHTML =
+                                            "Gráfica Elbow";
+                                        document.getElementById("plot").src =
+                                            "../img/agrupacion/k2Tempcodo.png?";
+
+                                        //Gráfica 3
+                                        document.getElementById("Title_grafic3")
+                                            .innerHTML =
+                                            "Gráfica de las Agrupaciones";
+                                        document.getElementById("graphic3").src =
+                                            "../img/agrupacion/k8Tempfinal.png";
+                                    })
+                                break;
 
                             }
 
@@ -323,7 +393,79 @@
                                         document.getElementById("graphic3").src =
                                             "../img/agrupacion/k2distfinal.png";
                                     })
-                                    break;
+                                break;
+                                case '4':
+                                    swal.fire({
+                                        title: "Ejecutando algoritmo...",
+                                        html: '',
+                                        timer: 2000,
+                                        timerProgressBar: true,
+                                        showConfirmButton: false,
+                                        animation: true,
+                                        onOpen: () => {
+                                            Swal.showLoading()
+                                        }
+                                    }).then((result) => {
+                                        cent.value =
+                                            ' [[0.34054998 0.04120126] [0.19346949 0.03587393] [0.0537699  0.03859923] [0.48517616 0.04147686]]'
+                                        sil.value = '0.545'
+                                        //Gráfica 1
+                                        document.getElementById("Title_grafic1")
+                                            .innerHTML =
+                                            "Distribución de los puntos";
+                                        document.getElementById("graphic").src =
+                                            "../img/agrupacion/k2distpuntos.png";
+                                        //Gráfica 2
+                                        document.getElementById("Title_grafic2")
+                                            .innerHTML =
+                                            "Gráfica Elbow";
+                                        document.getElementById("plot").src =
+                                            "../img/agrupacion/k2distcodo.png?";
+
+                                        //Gráfica 3
+                                        document.getElementById("Title_grafic3")
+                                            .innerHTML =
+                                            "Gráfica de las Agrupaciones";
+                                        document.getElementById("graphic3").src =
+                                            "../img/agrupacion/k4distfinal.png";
+                                    })
+                                break;
+                                case '7':
+                                    swal.fire({
+                                        title: "Ejecutando algoritmo...",
+                                        html: '',
+                                        timer: 2000,
+                                        timerProgressBar: true,
+                                        showConfirmButton: false,
+                                        animation: true,
+                                        onOpen: () => {
+                                            Swal.showLoading()
+                                        }
+                                    }).then((result) => {
+                                        cent.value =
+                                            '[[0.50516984 0.04136303] [0.2031619  0.03638826] [0.02953375 0.03670151] [0.03288319 0.90740349] [0.29054714 0.03990967] [0.1168638  0.0339113 ] [0.39137084 0.04171681]] '
+                                        sil.value = '0.527'
+                                        //Gráfica 1
+                                        document.getElementById("Title_grafic1")
+                                            .innerHTML =
+                                            "Distribución de los puntos";
+                                        document.getElementById("graphic").src =
+                                            "../img/agrupacion/k2distpuntos.png";
+                                        //Gráfica 2
+                                        document.getElementById("Title_grafic2")
+                                            .innerHTML =
+                                            "Gráfica Elbow";
+                                        document.getElementById("plot").src =
+                                            "../img/agrupacion/k2distcodo.png?";
+
+                                        //Gráfica 3
+                                        document.getElementById("Title_grafic3")
+                                            .innerHTML =
+                                            "Gráfica de las Agrupaciones";
+                                        document.getElementById("graphic3").src =
+                                            "../img/agrupacion/k7distfinal.png";
+                                    })
+                                break;
                             }
                         }
                         if (select == 'h') {
@@ -367,7 +509,42 @@
                                         document.getElementById("graphic3").src =
                                             "../img/agrupacion/k6tiempfinal.png";
                                     })
-                                    break;
+                                break;
+                                case '3':
+                                    swal.fire({
+                                        title: "Ejecutando algoritmo...",
+                                        html: '',
+                                        timer: 2000,
+                                        timerProgressBar: true,
+                                        showConfirmButton: false,
+                                        animation: true,
+                                        onOpen: () => {
+                                            Swal.showLoading()
+                                        }
+                                    }).then((result) => {
+                                        cent.value = ' [[0.24598343 0.33637172] [0.97857454 0.69887163] [0.97871553 0.33869795]]'
+                                        sil.value = '0.924'
+                                        //Gráfica 1
+                                        document.getElementById("Title_grafic1")
+                                            .innerHTML =
+                                            "Distribución de los puntos";
+                                        document.getElementById("graphic").src =
+                                            "../img/agrupacion/k6tiempuntos.png";
+                                        //Gráfica 2
+                                        document.getElementById("Title_grafic2")
+                                            .innerHTML =
+                                            "Gráfica Elbow";
+                                        document.getElementById("plot").src =
+                                            "../img/agrupacion/k6tiempcodo.png?";
+
+                                        //Gráfica 3
+                                        document.getElementById("Title_grafic3")
+                                            .innerHTML =
+                                            "Gráfica de las Agrupaciones";
+                                        document.getElementById("graphic3").src =
+                                            "../img/agrupacion/k3tiempfinal.png";
+                                    })
+                                break;
                             }
                         }
                     }
